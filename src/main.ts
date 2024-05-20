19f6e032c4fde7e6e3c4e909cc00421f7c0ec597
江品慧23866 <seirachiang-23866@testesunbank.com.tw>
2024-05-20 19:00:15 +0800

    import_fontawesome

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCloudSunRain, faTemperatureHigh } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add( faCloudSunRain, faTemperatureHigh );
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
